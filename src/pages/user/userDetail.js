9fb5d6adb7554015a24663abe69257765386207f
muhammadarslan3221 <muhammad.arslan@argonteq.com>
2022-06-06 16:52:03 +0500

    userManagement Changes

--- src/pages/user/userDetail.js
+++ src/pages/user/userDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Row, Card, Button, Container } from "@themesberg/react-bootstrap";
 import { Link } from "react-router-dom";
@@ -6,8 +6,38 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../../components/Navbar";
 import Profile1 from "../../assets/img/team/profile-picture-1.jpg";
 import { Routes } from "../../routes";
+import { useHistory, useLocation } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import { Grid } from "@mui/material";
+import moment from "moment";
 
-export default () => {
+const UserDetails = ({ }) => {
+
+  // const history = useHistory();
+  const location = useLocation()
+  const [userData] = useState(location.state.item)
+  // const {
+  //   location: { state },
+  // } = history;
+  const tabelRow = () => {
+    let arr = userData?.workExperience.map((item, index) => {
+      return {
+        id: index,
+        JobTitle: item?.jobTitle ? item?.jobTitle : "-",
+        employmenttype: item?.employmentType ? item?.employmentType : "-",
+        Location: item?.location ? item?.location : "-",
+        StartDate: item?.startDate
+          ? moment(item?.startDate).format("DD-MM-YYYY")
+          : "-",
+        EndDate: item?.endDate
+          ? moment(item?.endDate).format("DD-MM-YYYY")
+          : "-",
+        Detail: item?.details ? item?.details : "-",
+      };
+    });
+    return arr
+  }
+
   return (
     <>
       <Navbar module={"User Detail"} />
@@ -29,33 +59,25 @@ export default () => {
                 >
                   <Card.Body className="pb-2">
                     <Card.Img
-                      src={Profile1}
+                      // src={Profile1}
+                      src={userData?.profileImg ? userData?.profileImg : Profile1}
                       alt="Neil Portrait"
                       className="user-avatar large-avatar rounded-circle mx-auto mb-2"
                     />
                     <div className="border_bottom pb-3 mb-4">
                       <Card.Title>Neil Sims</Card.Title>
                       <Card.Subtitle className="fw-normal">
-                        Senior Software Engineer
-                      </Card.Subtitle>
-                      <Card.Text className="text-gray mb-2">
-                        Overall Rating
-                      </Card.Text>
-                      <Card.Text className="text-gray mb-0 starIcon">
-                        <FontAwesomeIcon icon={faStar} />
-                        <FontAwesomeIcon icon={faStar} />{" "}
-                        <FontAwesomeIcon icon={faStar} />
-                        <FontAwesomeIcon icon={faStar} />{" "}
-                        <FontAwesomeIcon icon={faStar} />
-                      </Card.Text>
-                      <Card.Text className="text-gray mb-2 reviews">
-                        (7 Reviews)
-                      </Card.Text>
+                        <Card.Text className="text-gray mb-2">
+                          <span className="text-black">
+                            {userData?.employmentType ? userData?.employmentType : " n/a"}
 
-                      <Card.Text className="text-gray mb-0">
-                        {" "}
-                        No. of Jobs Completed: <b> 25</b>
-                      </Card.Text>
+                          </span>
+                        </Card.Text>
+                      </Card.Subtitle>
+
+
+
+
                     </div>
 
                     <div className="">
@@ -88,18 +110,24 @@ export default () => {
                         Basic Information
                       </Card.Title>
                       <Card.Text className="text-gray mb-2">
-                        Full Name: <span className="text-black">Swet LLC</span>
+                        Full Name: <span className="text-black">
+                          {userData?.fullName ? userData?.fullName : " n/a"}
+                        </span>
                       </Card.Text>
                       <Card.Text className="text-gray mb-2">
                         Member Since:{" "}
                         <span className="text-black">24/02/2022</span>
                       </Card.Text>
+
                       <Card.Text className="text-gray mb-2">
-                        Work Status:{" "}
-                        <span className="text-black">Full-time</span>
+                        Description: <span className="text-black">
+                          {userData?.description ? userData?.description : " n/a"}
+                        </span>
                       </Card.Text>
                       <Card.Text className="text-gray mb-2">
-                        No. of Job Done: <span className="text-black">25</span>
+                        DOB: <span className="text-black">
+                          {userData?.dateofBirth ? userData?.dateofBirth : " n/a"}
+                        </span>
                       </Card.Text>
                     </div>
                     <div className="border_bottom pb-2 mb-4">
@@ -107,15 +135,49 @@ export default () => {
                         Contact Information
                       </Card.Title>
                       <Card.Text className="text-gray mb-2">
-                        Email: <span className="text-black">abc@xyz.com</span>
+                        Email: <span className="text-black">
+                          {userData?.fullName ? userData?.fullName : " n/a"}
+                        </span>
                       </Card.Text>
                       <Card.Text className="text-gray mb-2">
-                        Phone: <span className="text-black">000-1234-567</span>
+                        Phone: <span className="text-black">
+                          {userData?.phoneNumber ? userData?.phoneNumber : " n/a"}
+                        </span>
                       </Card.Text>
                       <Card.Text className="text-gray mb-2">
                         Address:{" "}
                         <span className="text-black">
-                          house 01, street 00, New York, USA
+
+                          {userData?.address ? userData?.address : " n/a"}
+                        </span>
+                      </Card.Text>
+
+                      <Card.Text className="text-gray mb-2">
+                        City:{" "}
+                        <span className="text-black">
+
+                          {userData?.city ? userData?.city : " n/a"}
+                        </span>
+                      </Card.Text>
+                      <Card.Text className="text-gray mb-2">
+                        Province:{" "}
+                        <span className="text-black">
+
+                          {userData?.province ? userData?.province : " n/a"}
+                        </span>
+                      </Card.Text>
+                      <Card.Text className="text-gray mb-2">
+                        Country Code:{" "}
+                        <span className="text-black">
+
+                          {userData?.countryCode ? userData?.countryCode : " n/a"}
+                        </span>
+                      </Card.Text>
+                      <Card.Text className="text-gray mb-2">
+                        postalCode:{" "}
+                        <span className="text-black">
+
+                          {userData?.postalCode ? userData?.postalCode : " n/a"}
                         </span>
                       </Card.Text>
                     </div>
@@ -124,12 +186,65 @@ export default () => {
                         Other Information
                       </Card.Title>
                       <Card.Text className="text-gray mb-2">
-                        Can Drive: <span className="text-black">Yes</span>
+                        Tools Available <span className="text-black">
+                          {userData?.toolsAvailable ? userData?.toolsAvailable : " n/a"}
+                        </span>
                       </Card.Text>
                       <Card.Text className="text-gray mb-2">
-                        Have Tools: <span className="text-black">Yes</span>
+                        Carrier Overview <span className="text-black">
+                          {userData?.carrierOverview ? userData?.carrierOverview : " n/a"}
+                        </span>
+                      </Card.Text>
+                      <Card.Text className="text-gray mb-2">
+                        Personal Attributes<span className="text-black">
+                          {userData?.personalAttributes ? userData?.personalAttributes : " n/a"}
+                        </span>
+                      </Card.Text>
+                      <Card.Text className="text-gray mb-2">
+                        Criminal Record <span className="text-black"><Card.Img
+                          // src={Profile1}
+                          src={userData?.criminalRecord ? userData?.criminalRecord : "--"}
+                          alt="Neil Portrait"
+                          className="user-avatar large-avatar rounded-circle mx-auto mb-2"
+                        /></span>
+                      </Card.Text>
+                      <Card.Text className="text-gray mb-2">
+                        frontId <span className="text-black"><Card.Img
+                          // src={Profile1}
+                          src={userData?.frontId ? userData?.frontId : "--"}
+                          alt="Neil Portrait"
+                          className="user-avatar large-avatar rounded-circle mx-auto mb-2"
+                        /></span>
+                      </Card.Text>
+
+                      <Card.Text className="text-gray mb-2">
+                        Back Id <span className="text-black"><Card.Img
+                          
+                          src={userData?.backId ? userData?.backId : "--"}
+                          alt="Neil Portrait"
+                          className="user-avatar large-avatar rounded-circle mx-auto mb-2"
+                        /></span>
                       </Card.Text>
                     </div>
+                    <Card.Title className="text-primary">
+                      Business Information
+                    </Card.Title>
+                    <Card.Text className="text-gray mb-2">
+                    businessLicense <span className="text-black"><Card.Img
+                        
+                        src={userData?.businessLicense ? userData?.businessLicense : "--"}
+                        alt="Neil Portrait"
+                        className="user-avatar large-avatar rounded-circle mx-auto mb-2"
+                      /></span>
+                    </Card.Text>
+                    <Card.Text className="text-gray mb-2">
+                    businessLogo<span className="text-black"><Card.Img
+                        
+                        src={userData?.businessLogo ? userData?.businessLogo : "--"}
+                        alt="Neil Portrait"
+                        className="user-avatar large-avatar rounded-circle mx-auto mb-2"
+                      /></span>
+                    </Card.Text>
                     <div className="pb-2 mb-4">
                       <Card.Title className="text-primary">
                         Categories
@@ -137,6 +252,26 @@ export default () => {
                       <Card.Text className="text-gray mb-2">
                         <span className="text-black">Plumber</span>
                       </Card.Text>
+                    </div>
+                    <div className="pb-2 mb-4">
+                      <Card.Title className="text-primary">
+                        Work Experience
+                      </Card.Title>
+                      <Grid style={{ height: 400, width: "100%", marginTop: "5%" }}>
+
+                        <DataGrid
+                          columns={[
+                            { field: "JobTitle" },
+                            { field: "employmenttype" },
+                            { field: "Location" },
+                            { field: "StartDate" },
+                            { field: "EndDate" },
+                            { field: "Detail" },
+                          ]}
+                          rows={tabelRow()}
+                        />
+
+                      </Grid>
                     </div>
                   </Card.Body>
                 </Card>
@@ -148,3 +283,4 @@ export default () => {
     </>
   );
 };
+export default UserDetails;
