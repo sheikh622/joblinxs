a78ecae5fc013c9032cd936621ee8d169ab74e3c
Usama <usama.akram@argonteq.com>
2022-06-10 18:01:32 +0500

    build

---
+++ build/precache-manifest.e03d7564ddf4021685a4907202c36800.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab97648fa5c85483eb63ef88b5c110d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "366cbd7d6610a71573c8",
+    "url": "/static/css/2.596eb61f.chunk.css"
+  },
+  {
+    "revision": "892ca7f053803c7f5d00",
+    "url": "/static/css/main.ca7f50cc.chunk.css"
+  },
+  {
+    "revision": "366cbd7d6610a71573c8",
+    "url": "/static/js/2.1ea2b18a.chunk.js"
+  },
+  {
+    "revision": "cb4bbd78297e8035f9d424be5ab143bc",
+    "url": "/static/js/2.1ea2b18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892ca7f053803c7f5d00",
+    "url": "/static/js/main.1310d31c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1310d31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d8882afbe634a4f3b5",
+    "url": "/static/js/runtime-main.20657f43.js"
+  },
+  {
+    "revision": "a925745ba251f066665a252601075e16",
+    "url": "/static/media/404.a925745b.svg"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "984f1da2348fd6c05ba0b8cd6ff5f586",
+    "url": "/static/media/profile.984f1da2.png"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "1217b8ee169b233ae80821ff35d74509",
+    "url": "/static/media/signin.1217b8ee.svg"
+  }
+]);
