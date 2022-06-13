ddb94848ef0c9411075e02916099dfaf3c5762a7
muhammadarslan3221 <muhammad.arslan@argonteq.com>
2022-06-13 12:48:59 +0500

    bug fixing branch

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -294,6 +294,7 @@ export default (props = {}) => {
                     }
                     link={Routes.Categories_Management.path}
                   />
+                  {/*  */}
                   {/* <NavItem
                     title="Job Management"
                     SVG={
