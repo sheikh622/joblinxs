d23fd0a3a77e9bdd387b66418f94bcc6e63f71f6
muhammadarslan3221 <muhammad.arslan@argonteq.com>
2022-06-14 10:31:36 +0500

    viewJob

--- src/Redux/JobManagement/reducers.js
+++ src/Redux/JobManagement/reducers.js
@@ -1,17 +1,14 @@
 import produce from "immer";
 import {
-
   CHANGE_USERS_ACTIVE_PAGE,
   GET_JOB_LISTING_SUCCESS,
 } from "./constants";
 const initialState = {
-
   UserActivePage: 0,
   getJobListing: [], 
  
 };
 const Job = produce((state = initialState, action) => {
-
   switch (action.type) {
     case CHANGE_USERS_ACTIVE_PAGE:
       state.UserActivePage = action.payload;
