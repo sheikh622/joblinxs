468e06ffe61d7b8f4cde73ab535e38eb9d8da2a7
hassam nasser <muhammad.hassam@argonteq.com>
2022-07-01 12:50:25 +0500

    favourite api data

--- src/Redux/addJob/actions.js
+++ src/Redux/addJob/actions.js
@@ -2,7 +2,9 @@ import {
     ADD_JOB,
     ADD_JOB_SUCCESS,
     GET_JOB_SUCCESS,
-    GET_JOB
+    GET_JOB,
+    FAVOURITE_JOB_LIST_SUCCESS,
+    FAVOURITE_JOB_LIST
   } from "./constants";
   
   export const getJobListing = (data) => ({
@@ -21,3 +23,12 @@ import {
     type:  GET_JOB_SUCCESS,
     payload: data,
   });
+  
+export const favouriteJobList = (data) => ({
+  type: FAVOURITE_JOB_LIST,
+  payload: data,
+});
+export const favouriteJobListSuccess = (data) => ({
+  type: FAVOURITE_JOB_LIST_SUCCESS,
+  payload: data,
+});
--- src/Redux/addJob/reducer.js
+++ src/Redux/addJob/reducer.js
@@ -1,11 +1,13 @@
 import produce from "immer";
 import {
   ADD_JOB_SUCCESS,
-  GET_JOB_SUCCESS
+  GET_JOB_SUCCESS,
+  FAVOURITE_JOB_LIST_SUCCESS
 } from "./constants";
 const initialState = {
   addJob: [],
   getJob: [],
+  favouriteJob:[],
 };
 const addJob = produce((state = initialState, action) => {
   switch (action.type) {
@@ -14,6 +16,9 @@ const addJob = produce((state = initialState, action) => {
       break;
     case GET_JOB_SUCCESS:
       state.getJob = action.payload;
+      break;
+      case FAVOURITE_JOB_LIST_SUCCESS:
+      state.favouriteJob = action.payload;
       break;
     default:
   }
