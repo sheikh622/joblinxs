2ed0f2488b05b27494cfb2e26f4ce58f516694a2
dawood-argon <87072902+dawood-argon@users.noreply.github.com>
2022-07-05 20:09:46 +0500

    Update axiosConfig.js

--- src/Routes/axiosConfig.js
+++ src/Routes/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { API_URL as BASE_URL_API } from "../utils/AppUtils";
 
 // const BASE_URL_API = "http://192.168.1.170:8000/api/v1/" 
-const BASE_URL_API = "http://ec2-52-14-121-20.us-east-2.compute.amazonaws.com:8000/api/v1/"
+const BASE_URL_API = "http://18.119.88.138:8000/api/v1/"
 
 const axiosConfig = axios.create({
   baseURL: BASE_URL_API,
