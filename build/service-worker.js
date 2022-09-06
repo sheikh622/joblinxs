5fcbe4b637f4c415c6b57cd197a72d6286471aa9
muhammadarslan3221 <muhammad.arslan@argonteq.com>
2022-09-06 15:29:52 +0500

    web reviews

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f30e6459b2cd6dd5a2efb737c45f09a6.js"
+  "/precache-manifest.e736d12fb4538ff0ba9e2c9e52dfd130.js"
 );
 
 self.addEventListener('message', (event) => {
