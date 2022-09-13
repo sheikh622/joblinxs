cb1dc9bd0f55826d5f3c13980a6f5891ad0d8ffa
malik hassam <78788462+hassnaseer@users.noreply.github.com>
2022-09-13 18:02:00 +0500

    Dev (#194)
    * longitude bug
    
    * longitude bug
    
    * to fixed
    
    * fixes bugs
    
    * deploy again
    
    * to fixed
    
    * fix bugs
    
    * resolce char last bug
    
    * resolve bugs
    
    * unit and rate field
    
    * unit and rate field
    
    * unit and rate field
    
    * bugs changes
    
    * changes
    
    * changes
    
    * emergency button
    
    * emergency button
    
    * foramt changes
    
    * emergency button
    
    * resolve bugs
    
    * category edit
    
    * category edit
    
    * zero unit field
    
    * zero unit field
    
    * Report Management
    
    * chat bugs fixes
    
    * terms and conditions
    
    * chat bugs fixes
    
    * chat bugs fixes
    
    * Privacy Policy
    
    * Privacy Policy
    
    * fix bug
    
    * toggle report
    
    * before push notification
    
    * before push notification
    
    * before push notification
    
    * chat new desgn
    
    * push notification
    
    * push notification
    
    * pull from dev
    
    * add build file
    
    * resolve chat bugs
    
    * url
    
    * latest build file
    
    * url changes
    
    * latest build
    
    * Feature chat (#151)
    
    * latest build file
    
    * url changes
    
    * latest build
    
    Co-authored-by: hassam nasser <muhammad.hassam@argonteq.com>
    
    * loaders implementation
    
    * certifications
    
    * certifications
    
    * add card data and implement stripe
    
    * latest build
    
    * resolce chat data
    
    * resolve chat error on live
    
    * resolve report issue
    
    * certification
    
    * report management bug
    
    * report management bug
    
    * change back button
    
    * change back button
    
    * scss changes
    
    * latest build added
    
    * Reviews
    
    * Reviews
    
    * conflict resolve
    
    * changes
    
    * provider reviews
    
    * provider reviews
    
    * web reviews
    
    * public or private route
    
    * latest code
    
    * latest build
    
    * responsiveness
    
    * responsiveness
    
    * pull from dev
    
    * resolve conflicts
    
    * changes
    
    * latest build
    
    * toggle bug
    
    * toggle bug
    
    * notification
    
    * notification
    
    * url chanhes
    
    * latest build
    
    * toggle changes
    
    * latest build
    
    * latest build
    
    * payment validation
    
    * payment validation
    
    * toggle bug
    
    * changes
    
    * changes
    
    * latest build
    
    * toggle bug
    
    * rate field
    
    * rate field
    
    * rate field
    
    * final bugs
    
    * bug changes
    
    * remove build
    
    * latest build add
    
    * latest url
    
    * latest build
    
    * url change
    
    * latest build
    
    * page refresh
    
    * data resolve
    
    * latest code
    
    * page refresh
    
    * console adds
    
    * url change
    
    * latest build
    
    * build
    
    * latest build
    
    * latest build
    
    * add job bug
    
    * trmove build
    
    * latest build
    
    * toaster bug
    
    * toaster bug
    
    * toaster bug
    
    * latest build
    
    * toaster bug
    
    * remove build
    
    * url change
    
    * latest build
    
    * report validation
    
    * report validation
    
    * changes
    
    * remove build
    
    * latest build
    
    * latest build
    
    Co-authored-by: muhammadarslan3221 <muhammad.arslan@argonteq.com>
    Co-authored-by: hassam nasser <muhammad.hassam@argonteq.com>
    Co-authored-by: HinaPerveen <89962039+HinaPerveen@users.noreply.github.com>
    Co-authored-by: muhammadarslan3221 <93127207+muhammadarslan3221@users.noreply.github.com>

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62f7f6bd1d84662f618ddb2d5fde75f3.js"
+  "/precache-manifest.e44b44a3362e16ca3a23bdd4a7eb7d87.js"
 );
 
 self.addEventListener('message', (event) => {
