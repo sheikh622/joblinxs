2d1141e0890adf915754b333d64ba8a0438b696e
hassam nasser <muhammad.hassam@argonteq.com>
2022-10-11 18:58:00 +0500

    latest build

---
+++ build/precache-manifest.8fb30995ec901ba043737dda88dcbfb9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb11dbf8fbbc932877371ca6933e2494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e07af5985f5638a6e9",
+    "url": "/static/css/2.151d21fd.chunk.css"
+  },
+  {
+    "revision": "af3f14fd5cf2864ff147",
+    "url": "/static/css/main.b7babae6.chunk.css"
+  },
+  {
+    "revision": "84e07af5985f5638a6e9",
+    "url": "/static/js/2.7caf87f4.chunk.js"
+  },
+  {
+    "revision": "fecefa696a77d54a29fd00ea58c76fd0",
+    "url": "/static/js/2.7caf87f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3f14fd5cf2864ff147",
+    "url": "/static/js/main.581e34b9.chunk.js"
+  },
+  {
+    "revision": "40d8882afbe634a4f3b5",
+    "url": "/static/js/runtime-main.20657f43.js"
+  },
+  {
+    "revision": "a925745ba251f066665a252601075e16",
+    "url": "/static/media/404.a925745b.svg"
+  },
+  {
+    "revision": "fd4c5bbe3fac3fc6f97d127e9c917823",
+    "url": "/static/media/american-express.fd4c5bbe.png"
+  },
+  {
+    "revision": "9b16179d18cf40fee9e64528b9c20b73",
+    "url": "/static/media/card.9b16179d.svg"
+  },
+  {
+    "revision": "15dc88dd64d1024bc9c14f58830b6ee0",
+    "url": "/static/media/diners-club.15dc88dd.png"
+  },
+  {
+    "revision": "7070503ce46e3b2e389141c3bd5c30fe",
+    "url": "/static/media/eyeON.7070503c.svg"
+  },
+  {
+    "revision": "77d74367998f559103a666dbebb70497",
+    "url": "/static/media/icon.77d74367.svg"
+  },
+  {
+    "revision": "91ef412e8a172040bd0a03eb005f1a6d",
+    "url": "/static/media/mastercard.91ef412e.jpeg"
+  },
+  {
+    "revision": "8b2f97f7283bdeb39650b0f5a7a065bd",
+    "url": "/static/media/phone-call.8b2f97f7.svg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "984f1da2348fd6c05ba0b8cd6ff5f586",
+    "url": "/static/media/profile.984f1da2.png"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "1217b8ee169b233ae80821ff35d74509",
+    "url": "/static/media/signin.1217b8ee.svg"
+  },
+  {
+    "revision": "a27755ba80948dd6bcc5a7bcf4e29e91",
+    "url": "/static/media/visa.a27755ba.png"
+  },
+  {
+    "revision": "c6f5d568ed5d72bb5cac0ff5bd6b1d8e",
+    "url": "/static/media/zoom.c6f5d568.svg"
+  }
+]);
