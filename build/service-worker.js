3de615e3d16f042858e0fe5673c9732cc58c5aa8
muhammad arslan <muhammad.arslan@argonteq.com>
2022-11-24 16:07:50 +0500

    final web bugs

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a73b6e224f80fcf2af2e30b0623d406.js"
+  "/precache-manifest.fc1b99225655cbed60571efc9fd69d0f.js"
 );
 
 self.addEventListener('message', (event) => {
